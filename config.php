c33e066cdaf6eb059f5235b246286188c6f01728
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2020-09-15 14:49:11 +1200

    ....

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On');  // change tp 'Off' when live
+
+define("DB_NAME","taskerj72129");
+define("DB_USERNAME","_taskerj72129");
+define("DB_PASSWORD","885c2809388c0b42a6f32724b400e9b6");
+define("DB_HOST","localhost");
+
+?>
